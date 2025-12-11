0d3e28424239fed7a64aa6161b22e2fbe65b6270
Callidem <brunomonti00@gmail.com>
2025-12-11 15:41:47 -0300

    avanço com a parte 2 do dia 2

--- 2025/Day02/Part02.go
+++ 2025/Day02/Part02.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	file, err := os.Open("data.txt")
+	file, err := os.Open("example.txt")
 	if err != nil {
 		fmt.Println("Erro ao abrir o arquivo:", err)
 		return
@@ -19,10 +20,12 @@ func main() {
 	const comma = ','
 	lineLen := 0
 	totalSum := 0
+	interval := "abc"
 	for {
 		line, err := arq.ReadString(trace)
+		interval = line
 		if len(line) > 0 {
-			fmt.Printf("Readline: %q\n", line)
+			//fmt.Printf("Readline: %q\n", line)
 		} else {
 			break
 		}
@@ -33,31 +36,45 @@ func main() {
 		}
 
 		n1, err := strconv.Atoi(string(line[0:lineLen]))
-		fmt.Printf("%d\n", n1)
 
 		line, err = arq.ReadString(comma)
+		interval += line
+		fmt.Printf("Interval: %s\n", interval)
 		if line[len(line)-1] == comma || line[len(line)-1] == trace {
 			lineLen = len(line) - 1
 		} else {
 			lineLen = len(line)
 		}
 		n2, err := strconv.Atoi(string(line[0:lineLen]))
-		fmt.Printf("%d\n", n2)
+		//ideia: pegar o primeiro caractere e comparar suas apari;'oes para identificar um padr'ao
+
 		for i := n1; i <= n2; i++ {
 			s1 := strconv.Itoa(i)
 			//fmt.Printf("Number: %s\n", s1)
-			h1 := "a"
-			if len(s1) > 2 {
-				h1 = s1[0:(len(s1) / 2)]
-				//fmt.Printf("Vai até %d\n", (len(s1) / 2))
-			} else {
-				h1 = s1[0 : len(s1)-1]
-			}
-			h2 := s1[(len(s1) / 2):]
-			//fmt.Printf("H1: %s H2: %s\n", h1, h2)
-			if h1 == h2 {
-				totalSum += i
-				//fmt.Printf("ACHEI IGUAL: %d\n", i)
+			char := s1[0]
+			patternFound := false
+			for j := 1; j < len(s1) && !patternFound; j++ {
+				if s1[j] == char {
+					index := strings.Index(s1[j:], string(char))
+					if index != -1 {
+						padrao := s1[0 : j+index]
+						fmt.Printf("Substring do possivel padrao: %s, j+index= %d\n", padrao, j+index)
+
+						for k := 0; k <= len(s1)-1; k += len(padrao) {
+							if k == len(s1) {
+								totalSum += i
+								patternFound = true
+								fmt.Printf("Padrao encontrado: %s no numero %s\n", padrao, s1)
+								break
+							}
+							if strings.Index(s1[k:], padrao) != 0 {
+								break
+							}
+
+						}
+					}
+					fmt.Printf("String: %s, Char: %c, j: %d, Index: %d\n", s1, char, j, index+j)
+				}
 			}
 		}
 		if err != nil {
